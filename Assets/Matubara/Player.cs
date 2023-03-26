3c15c0637280f5772e931d0cdd8b9fc89db14a72
MatubaraTooru <kurakura.ingen@gmail.com>
2023-03-26 12:35:18 +0900

    アイテムと交番のタグ追加　プレイヤーが交番に触れたら所持アイテムが0になるように変更

--- Assets/Matubara/Player.cs
+++ Assets/Matubara/Player.cs
@@ -8,15 +8,18 @@ public class Player : MonoBehaviour
     [SerializeField] float _moveSpeed;
     [SerializeField] int _inventorySize;
     [SerializeField] string _itemTag;
+    [SerializeField] float _reduce;
+    [SerializeField] string _kobanTag;
     Rigidbody2D _rb;
     float _h;
     float _v;
-    [SerializeField] GameObject[] _inventory;
-    int _index = 0;
+    [SerializeField] List<GameObject> _inventory;
+    // GameManager _gamemanager;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _inventory = new GameObject[_inventorySize];
+        _inventory = new List<GameObject>();
+        
     }
 
     void Update()
@@ -28,14 +31,19 @@ public class Player : MonoBehaviour
     {
         Vector2 dir = new Vector2(_h, _v).normalized;
         _rb.velocity = dir * _moveSpeed;
-        Debug.Log(_rb.velocity.magnitude);
+        Debug.Log(_moveSpeed);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(_itemTag) && _inventory[_inventory.Length - 1] == null)
+        if (collision.gameObject.CompareTag(_itemTag) && _inventory.Count < _inventorySize)
         {
-            _inventory[_index] = collision.gameObject;
-            _index++;
+            _inventory.Add(collision.gameObject);
+            // _moveSpeed -= _inventory.Count * _reduce;
+            //_moveSpeed *= 1f - (float)(_inventory.Count / _inventorySize);
+        }
+        else if (collision.gameObject.CompareTag(_kobanTag))
+        {
+            _inventory.RemoveRange(0, _inventory.Count);
         }
     }
 }
