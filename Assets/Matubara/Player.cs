5d2fe95402de470fa1d8f2fe01f16505037a2e28
MatubaraTooru <kurakura.ingen@gmail.com>
2023-03-26 14:16:31 +0900

    プレイヤープレハブ作成

--- Assets/Matubara/Player.cs
+++ Assets/Matubara/Player.cs
@@ -1,49 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed;
-    [SerializeField] int _inventorySize;
-    [SerializeField] string _itemTag;
-    [SerializeField] float _reduce;
-    [SerializeField] string _kobanTag;
+    [SerializeField, Header("プレイヤーの最大速度")] float _maxSpeed;
+    [SerializeField, Header("プレイヤーが落とし物を持てる最大数")] int _inventorySize;
     Rigidbody2D _rb;
     float _h;
     float _v;
-    [SerializeField] List<GameObject> _inventory;
-    // GameManager _gamemanager;
+    List<GameObject> _inventory;
+    float _moveSpeed;
+    bool _stun;
+    float _stunTimer = 0;
+    GameManager _gamemanager;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _inventory = new List<GameObject>();
-        
+        _gamemanager = GetComponent<GameManager>();
+        _moveSpeed = _maxSpeed;
     }
-
     void Update()
     {
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
+
+        if (_h > 0)
+        {
+            this.transform.localScale = new Vector3(-1 * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+        else if (_h < 0)
+        {
+            this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
     }
     private void FixedUpdate()
     {
         Vector2 dir = new Vector2(_h, _v).normalized;
         _rb.velocity = dir * _moveSpeed;
-        Debug.Log(_moveSpeed);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(_itemTag) && _inventory.Count < _inventorySize)
+        if (collision.gameObject.CompareTag("Item") && _inventory.Count < _inventorySize)
         {
+            var score = collision.gameObject.GetComponent<LostItemController>().Score;
             _inventory.Add(collision.gameObject);
-            // _moveSpeed -= _inventory.Count * _reduce;
-            //_moveSpeed *= 1f - (float)(_inventory.Count / _inventorySize);
+            _moveSpeed = Mathf.Clamp(_moveSpeed -= (float)_inventory.Count / (float)_inventorySize, 1f, _maxSpeed);
+            Debug.Log(_moveSpeed);
         }
-        else if (collision.gameObject.CompareTag(_kobanTag))
+        else if (collision.gameObject.CompareTag("Koban"))
         {
             _inventory.RemoveRange(0, _inventory.Count);
+            _moveSpeed = _maxSpeed;
         }
+    }
+    public void Stun(float time)
+    {
+        
     }
 }
