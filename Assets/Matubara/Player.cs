b4451a9176702f16072a47d7642b6c4d6403c539
MatubaraTooru <kurakura.ingen@gmail.com>
2023-03-26 11:33:32 +0900

    プレイヤーにアイテム回収機能追加

--- Assets/Matubara/Player.cs
+++ Assets/Matubara/Player.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
+    [SerializeField] int _inventorySize;
+    [SerializeField] string _itemTag;
     Rigidbody2D _rb;
     float _h;
     float _v;
+    [SerializeField] GameObject[] _inventory;
+    int _index = 0;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
+        _inventory = new GameObject[_inventorySize];
     }
 
     void Update()
@@ -24,5 +28,14 @@ public class Player : MonoBehaviour
     {
         Vector2 dir = new Vector2(_h, _v).normalized;
         _rb.velocity = dir * _moveSpeed;
+        Debug.Log(_rb.velocity.magnitude);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(_itemTag) && _inventory[_inventory.Length - 1] == null)
+        {
+            _inventory[_index] = collision.gameObject;
+            _index++;
+        }
     }
 }
