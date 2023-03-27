cafafd7ce7fcc37728b331cebe2ed77228683ec7
tatsupiko <identity2022@outlook.jp>
2023-03-27 10:20:33 +0900

    LostItemクラスを「Playerに接触したら非表示にするように」変更した。

--- Assets/Yoshizawa/Script/LostItemController.cs
+++ Assets/Yoshizawa/Script/LostItemController.cs
@@ -9,27 +9,11 @@ public class LostItemController : MonoBehaviour
     private int _score = 0;
     public int Score { get => _score; private set => _score = value; }
 
-    private Collider2D _collider2D = null;
-    private SpriteRenderer _spriteRenderer = null;
-
-    private void Start()
-    {
-        if (TryGetComponent(out Collider2D collider2D))
-        {
-            _collider2D = collider2D;
-            _collider2D.isTrigger = true;
-        }
-        if (TryGetComponent(out SpriteRenderer spriteRenderer)) _spriteRenderer = spriteRenderer;
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (_collider2D == null || _spriteRenderer == null) return;
-
         if (collision.gameObject.tag == "Player")
         {
-            _collider2D.enabled = false;
-            _spriteRenderer.sortingOrder = -1000;
+            gameObject.SetActive(false);
         }
     }
 }
