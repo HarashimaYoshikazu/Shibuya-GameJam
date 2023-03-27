bfa5a34acf8a0d7e8e421b8aa6333cd7db0f8f7c
tatsupiko <identity2022@outlook.jp>
2023-03-27 15:24:09 +0900

    落とし物を獲得したときにSEが再生されるように変更した。

--- Assets/Yoshizawa/Script/LostItemController.cs
+++ Assets/Yoshizawa/Script/LostItemController.cs
@@ -8,11 +8,14 @@ public class LostItemController : MonoBehaviour
     [SerializeField]
     private int _score = 0;
     public int Score { get => _score; private set => _score = value; }
+    [SerializeField]
+    private AudioClip _clip = null;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            HachikoSoundManager.Instance.PlayAudioClip(_clip);
             Destroy(gameObject);
         }
     }
