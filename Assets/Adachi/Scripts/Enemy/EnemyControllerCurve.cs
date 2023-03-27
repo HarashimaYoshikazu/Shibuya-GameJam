ff0de653aca4cca2fa568b2910b72e86b449de71
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-03-27 13:50:39 +0900

    曲線に曲がる一般人を作った

--- Assets/Adachi/Scripts/Enemy/EnemyControllerCurve.cs
+++ Assets/Adachi/Scripts/Enemy/EnemyControllerCurve.cs
@@ -9,12 +9,12 @@ public class EnemyControllerCurve : EnemyBase
     [Header("”¼Œa")]
     private float _radius = 1f;
 
-    private const float OFFSET = 1f;
+    private const float OFFSET = 4f;
 
     protected override void Awake()
     {
         base.Awake();
-        OnMove();
+        //OnMove();
     }
 
     public async override void OnMove()
@@ -24,72 +24,21 @@ public class EnemyControllerCurve : EnemyBase
         var end = max - min;
         var _deg = 0f;
 
-        var pos = _twoPos.MaxValue.position;
-        var pattern0 = transform.position.x > pos.x && transform.position.y > pos.y;
-        var pattern1 = transform.position.x > pos.x && transform.position.y < pos.y;
-        var pattern2 = transform.position.x < pos.x && transform.position.y > pos.y;
-        var pattern3 = transform.position.x < pos.x && transform.position.y < pos.y;
-        if (pattern0)
-        {
-            while (true)
-            {
-                float time = 1.0f;
-                float f = 1.0f / time;
-                var x = _radius * Mathf.Cos(Time.time);
-                var y = _radius * Mathf.Sin(2 * Mathf.PI * f * Time.time);
-                _rb.velocity = new Vector2(x, y) * end.magnitude;
-                _deg += 2 * Mathf.PI / 360f;
-                if (transform.position.x < pos.x && transform.position.y < pos.y) break;
-                await UniTask.NextFrame();
-            }
-            pos = new Vector3(_twoPos.MaxValue.position.x, _twoPos.MaxValue.position.y, _twoPos.MaxValue.position.z);
-        }
-        else if (pattern1)
-        {
-            while (true)
-            {
-                float time = 1.0f;
-                float f = 1.0f / time;
-                var x = _radius * Mathf.Cos(Time.time);
-                var y = _radius * Mathf.Sin(2 * Mathf.PI * f * Time.time);
-                _rb.velocity = new Vector2(x, y) * end.magnitude;
-                _deg += 2 * Mathf.PI / 180f;
-                if (transform.position.x < pos.x && transform.position.y > pos.y) break;
-                await UniTask.NextFrame();
-            }
-            pos = new Vector3(_twoPos.MaxValue.position.x, _twoPos.MaxValue.position.y, _twoPos.MaxValue.position.z);
-        }
-        else if (pattern2)
-        {
-            while (true)
-            {
-                float time = 1.0f;
-                float f = 1.0f / time;
-                var x = _radius * Mathf.Cos(Time.time);
-                var y = _radius * Mathf.Sin(2 * Mathf.PI * f * Time.time);
-                _rb.velocity = new Vector2(x, y) * end.magnitude;
-                _deg += 2 * Mathf.PI / 180f;
-                if (transform.position.x > pos.x && transform.position.y < pos.y) break;
-                await UniTask.NextFrame();
-            }
-            pos = new Vector3(_twoPos.MaxValue.position.x, _twoPos.MaxValue.position.y, _twoPos.MaxValue.position.z);
-        }
-        else if (pattern3)
-        {
-            while (true)
-            {
-                float time = 1.0f;
-                float f = 1.0f / time;
-                var x = _radius * Mathf.Cos(Time.time);
-                var y = _radius * Mathf.Sin(2 * Mathf.PI * f * Time.time);
-                _rb.velocity = new Vector2(x, y) * end.magnitude;
-                _deg += 2 * Mathf.PI / 180f;
-                if (transform.position.x > pos.x && transform.position.y > pos.y) break;
-                await UniTask.NextFrame();
-            }
-            pos = new Vector3(_twoPos.MaxValue.position.x, _twoPos.MaxValue.position.y, _twoPos.MaxValue.position.z);
-        }
+        float _timer = 0f;
+        _rb.velocity = end.normalized;
 
-        Destroy(gameObject);
+        while (true)
+        {
+            float t = 1.0f;
+            float f = 1.0f / t;
+            var x = _radius * Mathf.Cos(_timer);
+            var y = _radius * Mathf.Sin(2 * Mathf.PI * f * _timer);
+
+            _rb.velocity = end.normalized + new Vector3(x, y);
+            _deg += 2 * Mathf.PI / 360f;
+            _timer += Time.deltaTime / OFFSET;
+            SetFlip(_rb);
+            await UniTask.NextFrame();
+        }
     }
 }
